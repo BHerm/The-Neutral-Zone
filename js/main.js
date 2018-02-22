91670839bc07ec4e543dd7df5bba299dd0fbb81e
Brad Hermann <bsh6268@rit.edu>
2018-02-21 23:41:37 -0500

    eventlistener when area outside modal is clicked

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ var closeBtn = document.getElementsByClassName('close-button')[0];
 
 //function to open openmodal
 function openmodal(){
-  modal.style.display= 'block';
+  modal.style.display = 'block';
 }
 
 //function to close openmodal
@@ -18,6 +18,14 @@ function closemodal(){
   modal.style.display = 'none';
 }
 
+//close modal if click outside box
+function outsideClick(e){
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
+
 //listen for click
 openBtn.addEventListener('click', openmodal);
 closeBtn.addEventListener('click', closemodal);
+window.addEventListener('click', outsideClick);
